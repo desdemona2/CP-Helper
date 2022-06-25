11190027791ece8cdc48f034d8f84075a9e695be
Bhavuk <bhavuksharma2202@gmail.com>
2022-06-25 14:52:47 +0530

    onClickListeners for activity_main

--- app/src/main/java/com/redheadhammer/cphelper/MainActivity.java
+++ app/src/main/java/com/redheadhammer/cphelper/MainActivity.java
@@ -1,14 +1,39 @@
 package com.redheadhammer.cphelper;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("Select One item");
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.createArray).setOnClickListener(
+                this::CreateArray);
+        findViewById(R.id.xorCalculator).setOnClickListener(
+                this::XorCalculator);
+        findViewById(R.id.BinaryForm).setOnClickListener(
+                this::BinaryRepresentation);
+        findViewById(R.id.gcdCalculator).setOnClickListener(
+                this::GcdCalculator);
+    }
+
+    public void CreateArray(View view) {
+    }
+
+    public void XorCalculator(View view) {
+    }
+
+    public void BinaryRepresentation(View view) {
+    }
+
+    public void GcdCalculator(View view) {
     }
 }
