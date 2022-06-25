c7c79e0de61331077968c9b52b5f2433e3632258
Bhavuk <bhavuksharma2202@gmail.com>
2022-06-25 22:05:01 +0530

    random array generator layout

---
+++ app/src/main/java/com/redheadhammer/cphelper/RandomArray.java
@@ -0,0 +1,14 @@
+package com.redheadhammer.cphelper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RandomArray extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_random_array);
+    }
+}
